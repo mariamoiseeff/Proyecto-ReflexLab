4dc03096fbefb9f872e047eaf9cecd3d3e9e56ea
mariamoiseeff <mmoiseeff@udesa.edu.ar>
2026-03-27 16:15:15 -0300

    archivos de metricas y procesamiento de datos en la carpeta src

---
+++ src./metricas.py
@@ -0,0 +1,33 @@
+#metricas
+def calcular_tiempo_reaccion_promedio(datos: list) -> float:
+
+   '''
+promedio de la lista de tiempos de reaccion de un pariticpante
+    
+
+    Parameters
+    ----------
+    datos : list
+        lista de tiempos de reaccion .
+
+    Returns
+    -------
+    float
+        promedio: suma de todos los tiempos de reaccion dividido la cantidad.
+
+    '''
+def calcular_tasa_error(datos: list) -> float:
+    '''
+    calcula  proporción de respuestas incorrectas
+
+    Parameters
+    ----------
+    datos : list
+        lista de respuestas de un participante.
+
+    Returns
+    -------
+    float
+        resultado de division de respuestas incorrectas / respuestas totales.
+
+    '''
---
+++ src./procesamiento_datos.py
@@ -0,0 +1,18 @@
+#procesamiento datos
+def filtrar_por_participante(datos, id_participante:):
+'''
+    seleccionar los datos correspondientes a un participante y lo devuelve
+
+    Parameters
+    ----------
+    datos : list
+        lista de diccionarios de todos los datos parseados.
+    id_participante : int
+        numero de participante.
+
+    Returns
+    -------
+    diccionario con todos los datos de un participante
+    
+    '''
+    
