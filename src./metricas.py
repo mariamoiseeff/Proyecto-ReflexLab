a8ddee5871f5bca6757c4ab7201c12fba0cb69fe
claraantelocernadas <cantelocernadas@udesa.edu.ar>
2026-04-04 17:35:22 -0300

    Update metricas.py

--- src./metricas.py
+++ src./metricas.py
@@ -15,14 +15,14 @@ def calcular_tiempo_reaccion_promedio(datos):
     float
         promedio: suma de todos los tiempos de reaccion dividido la cantidad.
     '''
-    promedios_tiempos_reaccion = []
-    for registro_participante in datos: #me agarra diccionarios de registro de un participante
+   promedios_tiempos_reaccion = []
+   for registro_participante in datos: #me agarra diccionarios de registro de un participante
         tiempos_reaccion = registro_participante["tiempo de reaccion"]
         promedio = sum(tiempos_reaccion) / len(tiempos_reaccion)
         promedios_tiempos_reaccion.append(promedio)
     
-    promedio_todos = sum(promedios_tiempos_reaccion) / len(promedios_tiempos_reaccion)
-    return promedio_todos
+   promedio_todos = sum(promedios_tiempos_reaccion) / len(promedios_tiempos_reaccion)
+   return promedio_todos
         
     
     
