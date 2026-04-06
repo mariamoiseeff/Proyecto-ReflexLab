f2ad7ba164b3bc9dbcf69704881a065b945066d9
claraantelocernadas <cantelocernadas@udesa.edu.ar>
2026-04-06 14:36:02 -0300

    Update main

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-# programa principal 
-# Se encarga de orquestar todo el flujo del programa
-#cargar → validar → procesar → analizar → mostrar
+from src.carga_datos import cargar_datos
+from src.procesamiento_datos import filtrar_por_participante
+from src.metricas import calcular_tiempo_reaccion_promedio
+from src.metricas import calcular_tasa_error
+
+
+datos_validos = cargar_datos("datos.csv")
+
+id_participante = input("ID de participante: ")
+participante_pedido = filtrar_por_participante(datos_validos, id_participante)
+print(participante_pedido)        
+
+promedio_general = calcular_tiempo_reaccion_promedio(datos_validos)
+print(f"El promedio general es: {promedio_general}")
+
+tasa_error_general = calcular_tasa_error(datos_validos)
+print(f"La tasa de error general es: {tasa_error_general}")
