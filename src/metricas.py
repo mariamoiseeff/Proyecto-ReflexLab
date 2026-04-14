111f9e4b1056124b5b5176adb5868331df0213da
sofidavidov <sdavidov@udesa.edu.ar>
2026-04-14 11:31:40 -0300

    cambio metricas

--- src/metricas.py
+++ src/metricas.py
@@ -1,10 +1,9 @@
 #metricas
-def calcular_tiempo_reaccion_promedio(datos): 
+def calcular_tiempo_reaccion_promedio(datos,id_buscado): 
     
    '''
     promedio de tiempos de reaccion de todos los participantes
     
-
     Parameters
     ----------
     datos : list
@@ -15,18 +14,17 @@ def calcular_tiempo_reaccion_promedio(datos):
     float
         promedio: suma de todos los tiempos de reaccion dividido la cantidad.
     '''
-   promedios_tiempos_reaccion = []
-   for registro_participante in datos: #me agarra diccionarios de registro de un participante
-        tiempos_reaccion = registro_participante["tiempo de reaccion"]
-        promedio = sum(tiempos_reaccion) / len(tiempos_reaccion)
-        promedios_tiempos_reaccion.append(promedio)
+   for registro_participante in datos:
+        if registro_participante["id"] == id_buscado:
+            
+            tiempos_reaccion = registro_participante["tiempo de reaccion"]
+            promedio = sum(tiempos_reaccion) / len(tiempos_reaccion)
+            
+            return promedio
     
-   promedio_todos = sum(promedios_tiempos_reaccion) / len(promedios_tiempos_reaccion)
-   return promedio_todos
+   return 0           
         
-    
-    
-def calcular_tasa_error(datos: list) -> float:
+def calcular_tasa_error(datos,id_buscado):
     '''
     calcula  proporción de respuestas incorrectas
 
@@ -41,22 +39,27 @@ def calcular_tasa_error(datos: list) -> float:
         resultado de division de respuestas incorrectas / respuestas totales.
 
     '''
-    
-    if len(datos) == 0:
-        return 0
-    
-    cantidad_incorrectas = 0 
-    
+     
+    cantidad_incorrectas = 0
+    cantidad_total = 0
+
     for registro in datos:
-        if registro ["resultado_respuesta"]== "incorrecto":
-            cantidad_incorrectas += 1 
+        if registro["id"] == id_buscado:
             
-    tasa_error = cantidad_incorrectas / len(datos)
-    return tasa_error
+            cantidad_total += 1
+            
+            if registro["resultado_respuesta"] == "incorrecto":
+                cantidad_incorrectas += 1
+
+    if cantidad_total == 0:
+        return 0
+
+    tasa_error = cantidad_incorrectas / cantidad_total
+    return tasa_error 
             
             
             
-            
+             
 
             
             
