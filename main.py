af3e7b0f09a96f5529f0ae1ecc0230de3cf6be44
sofidavidov <sdavidov@udesa.edu.ar>
2026-04-16 13:01:55 -0300

    main

--- main.py
+++ main.py
@@ -4,29 +4,46 @@ from src.metricas import calcular_tiempo_reaccion_promedio
 from src.metricas import calcular_tasa_error
 
 ruta = "datos/ReflexLab_mock_data.csv"
-datos_validos = cargar_datos(ruta) #nose si tenemos que usar try-except aca porque tenemos un try-expect dentro de carga datos que corresponde a la validacion
 
+# Cargar datos
 try:
-    id_participante = int(input("ID de participante: ")) #tenemos que validar que el id sea valido?
-    if id_participante <= 0:
-        raise ValueError("El id del participante no puede ser negativo ")
-except:
-    raise ValueError("El id es invalido")
+    datos_validos = cargar_datos(ruta)
+except ValueError as e:
+    print(f"Error al cargar datos: {e}")  
+    datos_validos = None
 
-try: 
-    participante_pedido = filtrar_por_participante(datos_validos, id_participante)
-except ValueError as e: 
-    print(e)
-else: 
-    print(participante_pedido)        
+if datos_validos is not None:
 
-#haria bloque try-except porque vamos a manejar errores dentro del promedio
-promedio_id_participante = calcular_tiempo_reaccion_promedio(datos_validos, id_participante)
- #cambiar el argumento de estas funciones para que sea participante_pedido
-print(f"El promedio del participante es: {promedio_id_participante}") 
+    try:
+        id_participante = int(input("ID de participante: "))
+        if id_participante <= 0:
+            raise ValueError("El id del participante no puede ser negativo o cero")
+    except ValueError as e:
+        print(f"Error: {e}")
+        id_participante = None
 
-#otro bloque try-except poruqe vamos a manejar errorer dentro de calcular tasa error. 
-tasa_error_id_participante = calcular_tasa_error(datos_validos, id_participante)
- #cambiar el argumento de estas funciones para que sea participante_pedido
-print(f"La tasa de error del participante es: {tasa_error_id_participante}")
+    if id_participante is not None:
+        try:
+            # Filtrar participante
+            participante_pedido = filtrar_por_participante(datos_validos, id_participante)
+        except ValueError as e:
+            print (f"Error: {e}")
+            participante_pedido = None
+            
+        if participante_pedido is not None:
+
+            # Calcular promedio
+           try:             
+               promedio = calcular_tiempo_reaccion_promedio(participante_pedido)
+               print(f"El promedio del participante es: {promedio} ms") 
+           except ValueError as e:
+               print (f"Error: {e}")    
+              
+            # Calcular tasa de error 
+           try:                
+                tasa_error = calcular_tasa_error(participante_pedido)
+                print(f"La tasa de error del participante es: {tasa_error}%")
+
+           except ValueError as e: 
+                print(f"Error: {e}") 
 
